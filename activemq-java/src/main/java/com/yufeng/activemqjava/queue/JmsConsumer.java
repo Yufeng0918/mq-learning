1e73a03b02259a2e2c6c7525ac2c84e54f6aea71
daiyu <jiangyufeng.0918@gmail.com>
2020-01-31 17:45:38 +0800

    init

--- activemq-java/src/main/java/com/yufeng/activemqjava/queue/JmsConsumer.java
+++ activemq-java/src/main/java/com/yufeng/activemqjava/queue/JmsConsumer.java
@@ -3,6 +3,7 @@ package com.yufeng.activemqjava.queue;
 import org.apache.activemq.ActiveMQSslConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 import java.util.stream.IntStream;
 
 /**
@@ -15,7 +16,7 @@ public class JmsConsumer {
     public static final String ACTIVEMQ_URL = "tcp://127.0.0.1:61616";
     public static final String QUEUE_NAME = "queue01";
 
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException {
 
         ConnectionFactory connectionFactory = new ActiveMQSslConnectionFactory(ACTIVEMQ_URL);
         Connection connection = connectionFactory.createConnection();
@@ -24,10 +25,22 @@ public class JmsConsumer {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Queue destination = session.createQueue(QUEUE_NAME);
         MessageConsumer messageConsumer = session.createConsumer(destination);
-        while (true) {
 
-            TextMessage message = (TextMessage) messageConsumer.receive();
-            System.out.println("Received: " + message.getText());
-        }
+//        while (true) {
+//            TextMessage message = (TextMessage) messageConsumer.receive(4000);
+//            System.out.println("Received: " + message.getText());
+//        }
+
+        messageConsumer.setMessageListener(message -> {
+            try {
+                System.out.println(((TextMessage) message).getText());
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        });
+        System.in.read();
+        messageConsumer.close();
+        session.close();
+        connection.close();
     }
 }
