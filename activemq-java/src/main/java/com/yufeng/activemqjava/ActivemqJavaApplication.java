8abaf30912b64c49600b9a0324af4b3439c20082
daiyu <jiangyufeng.0918@gmail.com>
2020-02-14 16:22:59 +0800

    notes

--- activemq-java/src/main/java/com/yufeng/activemqjava/ActivemqJavaApplication.java
+++ activemq-java/src/main/java/com/yufeng/activemqjava/ActivemqJavaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,14 +14,44 @@ public class ActivemqJavaApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(ActivemqJavaApplication.class, args);
 
-		int[] numbers = new int[3];
-		Arrays.stream(numbers).mapToObj(i -> String.valueOf(i)).collect(Collectors.joining(", "));
-//		IntStream.range(0, numbers.length).forEach(i -> numbers[i] = random.nextInt(10000));
-
-		int a = 10000;
-		int b = 9000;
-		long c = a * b;
-		System.out.println(c);
+		PriorityQueue<Node> priorityQueue = new PriorityQueue<>((o1, o2) -> Double.valueOf(o1.x).compareTo(Double.valueOf(o2.cost)));
 	}
 
+
+	class Node {
+
+		int x;
+		int y;
+		double cost;
+		Node parent;
+
+		public Node(int x, int y, double cost, Node parent) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+			this.parent = parent;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			return sb.toString();
+		}
+
+
+		@Override
+		public boolean equals(Object o) {
+
+			if (o == this) {
+				return true;
+			}
+
+			if (!(o instanceof Node)) {
+				return false;
+			}
+
+			Node c = (Node) o;
+			return c.x == this.x && c.y == this.y;
+		}
+	}
 }
